afcc9372a34f4c180e61a48e6eb938b37ef72a32
ivan <rybnikovv12345@gmail.com>
2021-10-07 11:00:40 +0400

    adding register component + adding bootstrap (front)

--- QuestionnaireFrontend/src/app/app.module.ts
+++ QuestionnaireFrontend/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { authInterceptorProviders } from './helpers/auth.interceptor';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
