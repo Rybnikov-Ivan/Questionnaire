2c4f330d02358c09a9623b3aa59f6e25a5c8a361
ivan <rybnikovv12345@gmail.com>
2021-10-09 11:03:03 +0400

    fix bags

--- Questionnaire/src/main/java/com/OpenCodeBackend/Questionnaire/QuestionnaireApplication.java
+++ Questionnaire/src/main/java/com/OpenCodeBackend/Questionnaire/QuestionnaireApplication.java
@@ -2,10 +2,8 @@ package com.OpenCodeBackend.Questionnaire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.OpenCodeBackend.Questionnaire.security"})
 public class QuestionnaireApplication {
 
 	public static void main(String[] args) {
