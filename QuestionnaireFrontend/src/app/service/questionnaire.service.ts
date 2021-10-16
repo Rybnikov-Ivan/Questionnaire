da9520c820550a7fcff0dd029ca1ae0e80445aee
ivan <rybnikovv12345@gmail.com>
2021-10-16 17:37:31 +0400

    add questionnaire service

---
+++ QuestionnaireFrontend/src/app/service/questionnaire.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Questionnaire } from '../models/questionnaire';
+
+const URL = 'http://localhost:8080/admin/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionnaireService {
+
+  constructor(private http: HttpClient) { }
+
+  saveQuestionnaire(questionnaire: Questionnaire): Observable<Questionnaire> {
+    return this.http.post<Questionnaire>(URL+'designer',
+    questionnaire,
+    httpOptions);
+  }
+}
