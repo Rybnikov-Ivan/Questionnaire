b65e5301248cd452cf3ad925b8df0b615c82035e
ivan <rybnikovv12345@gmail.com>
2021-10-17 11:41:17 +0400

    update questionnaire-designer (fix bags)

--- QuestionnaireFrontend/src/app/board-admin/questionnaire-designer/questionnaire-designer.component.ts
+++ QuestionnaireFrontend/src/app/board-admin/questionnaire-designer/questionnaire-designer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { Questionnaire } from 'src/app/models/questionnaire';
 import { QuestionnaireService } from 'src/app/service/questionnaire.service';
 
@@ -9,65 +9,105 @@ import { QuestionnaireService } from 'src/app/service/questionnaire.service';
   styleUrls: ['./questionnaire-designer.component.css']
 })
 export class QuestionnaireDesignerComponent implements OnInit {
-
   questionForm!: FormGroup;
 
-  oneSelectedAnswer: boolean = false;
-  severalSelectedAnswers: boolean = false;
-
-  constructor( private fb: FormBuilder, private questionnaireService: QuestionnaireService ) {
-    this._createForm()
-   }
-
-  private _createForm(){
-    this.questionForm = this.fb.group({
-      questions: this.fb.array([]),
-      answers: this.fb.array([])
-    })
+  constructor( private fb: FormBuilder, private questionnaireService: QuestionnaireService )
+  {
+    this.initForm()
   }
 
   ngOnInit(): void {
   }
 
-  onChange(event: any){
-    if(event.target.value == "1"){
-      this.oneSelectedAnswer = true;
-      this.severalSelectedAnswers = false;
-    }
-    else if(event.target.value == "2") {
-      this.severalSelectedAnswers = true;
-      this.oneSelectedAnswer = false;
-    } else {
-      this.oneSelectedAnswer = false;
-      this.severalSelectedAnswers = false;
-    }
+  /**
+   * Initialize the form
+   */
+  private initForm(){
+    this.questionForm = this.fb.group({
+      title: [""],
+      description: [""],
+      questions: this.fb.array([this.getQuestionItem()])
+    })
   }
 
+  /**
+   * Create a form group for Question
+   */
+  private getQuestionItem(){
+    return this.fb.group({
+      question: [""],
+      typeAnswers: [""],
+      answers: this.fb.array([this.getAnswerItem()])
+    })
+  }
+
+  /**
+   * Create a form group for Answer
+   */
+  private getAnswerItem(){
+    return this.fb.group({
+      answer: [""]
+    });
+  }
+
+  /**
+   * Getter for questions item as FormArray
+   */
   get questions() {
     return (this.questionForm.get('questions') as FormArray);
   }
 
-  addQuestion() {
-    this.questions.push(this.fb.control(''))
+  /**
+   * Get answers of a particular index as FormArray
+   * @param questionIndex - index of the question
+   * @returns
+   */
+  getAnswersFormArray(questionIndex: number){
+    return this.questions.controls[questionIndex]?.get("answers") as FormArray;
   }
 
+  /**
+   * Get Form Controls of the answers array
+   * @param answerIndex - index of the question
+   * @returns
+   */
+  getAnswerControls(questionIndex: number){
+    return this.getAnswersFormArray(questionIndex)?.controls;
+  }
+
+  /**
+   * Add a answer item to existing form array
+   */
+  addQuestion() {
+    this.questions.push(this.getQuestionItem());
+  }
+
+  /**
+   * Remove a question item from the form array
+   * @param index - index of the question item to be removed
+   */
   removeQuestionAt(index: number){
     this.questions.removeAt(index);
   }
 
-  get answers(){
-    return (this.questionForm.get('answers') as FormArray);
+  /**
+   * Add answer to the selected question
+   * @param questionIndex - index of the question selected
+   */
+  addAnswer(questionIndex: number){
+    this.getAnswersFormArray(questionIndex)?.push(this.getAnswerItem());
   }
 
-  addAnswer() {
-    this.answers.push(this.fb.control(''));
+  /**
+   * Remove a answer from the question
+   * @param questionIndex - index of the selected question
+   * @param answerIndex - index of answer to remove
+   */
+  removeAnswerAt(questionIndex: number, answerIndex: number){
+    this.getAnswersFormArray(questionIndex).removeAt(answerIndex);
   }
 
-  removeAnswerAt(index: number){
-    this.answers.removeAt(index);
-  }
-
-  save(form: FormGroup): void {
+  createQuestionnaire(form: FormGroup): void {
     this.questionnaireService.saveQuestionnaire(form.value).subscribe(
       (response: Questionnaire) => {}
     )
